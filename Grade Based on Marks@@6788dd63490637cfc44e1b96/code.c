28378abe51a4dee632f6c4019f636a43d70048f1
Renu0618 <renu0618.becse24@chitkara.edu.in>
2025-02-16 14:14:42 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -5,11 +5,11 @@ int main()
     scanf("%d",&n);
     if(n>=90) {
         printf("A");
-    } else if (n>=80 and n<90:) {
+    } else if (n>=80 && n<90:) {
         printf("B");
-    } else if (n>=70 and n<80:) {
+    } else if (n>=70 && n<80:) {
         printf("C");
-    } else if (n>=60 and n<70:) {
+    } else if (n>=60 && n<70:) {
         printf("D");
     } else (n<60:) {
         printf("F");
