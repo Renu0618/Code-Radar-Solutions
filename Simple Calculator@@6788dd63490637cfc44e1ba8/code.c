3190a18bc693622c00b25c5efb39881f423b35bf
Renu0618 <renu0618.becse24@chitkara.edu.in>
2025-03-18 09:30:24 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,7 +4,7 @@ int main()
     int a,b;
     scanf("%d %d",&a,&b);
     char operator;
-    scanf("%c",&operator);
+    scanf(" %c",&operator);
     if(operator == '+') {
         printf("%d",a+b);
     }
