047010dc96ac356d2060b34e9103848feb2c48a1
Renu0618 <renu0618.becse24@chitkara.edu.in>
2025-03-30 12:26:42 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
-int fib(int n);
+int fibonacciSeries(int n);
 int main()
 {
     int n;
     scanf("%d",&n);
 
-    fib(n);
+    fibonacciSeries(n);
 
     return 0;
 }
-int fib(int n) {
+int fibonacciSeries(int n) {
     if(n<=0) {
         return 0;
     }
