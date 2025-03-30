2896568d724c966c013ab49f14ee0952056ea309
Renu0618 <renu0618.becse24@chitkara.edu.in>
2025-03-30 13:36:28 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,9 +3,11 @@ int fibonacciSeries(int N) {
     int b=1;
     for(int i=0;i<=N;i++) {
         printf("%d",a);
-        int c=a+b;
+        int c=a;
         a=b;
         b=c;
+        b=a+b;
+        
     }
 }
 
